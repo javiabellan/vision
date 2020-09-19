7364e0709212f05aac1276923bc68da77f1569bc
Javi <franfdk17@gmail.com>
2020-09-19 16:10:25 +0200

    videoCapture.py

--- nbs/VideoCapture.py
+++ nbs/VideoCapture.py
@@ -1,21 +1,54 @@
 import numpy as np
+import argparse
 import cv2
+import time
+import os
 
-url = "http://12.216.61.98:82/mjpg/video.mjpg" # Perretes
-cap = cv2.VideoCapture(url)
+
+# Parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video", required=True, help="path to input video file")
+args = vars(ap.parse_args())
+
+# open a pointer to the video stream and start the FPS timer
+stream = cv2.VideoCapture(args["video"])
+
+# Resolution
+W = int(stream.get(cv2.CAP_PROP_FRAME_WIDTH))
+H = int(stream.get(cv2.CAP_PROP_FRAME_HEIGHT))
+resolution_str = "Res: "+str(W)+"x"+str(H)
+
+# FPS
+frameCounter = 0
+timeBegin   = time.time();
+average_fps = 0
+
 
 while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+	# Capture frame-by-frame
+	ok, frame = stream.read() # Read method both GET and DECODE frame (bad practice)
 
-    # Our operations on the frame come here
-    #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	if not ok:
+		break
 
-    # Display the resulting frame
-    cv2.imshow('frame',frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+	frameCounter += 1
+
+	if (frameCounter % 10 == 0):
+		elapsedTime = time.time() - timeBegin
+		average_fps = round(frameCounter / elapsedTime, 2)
+		
+		os.system('clear')
+		print(resolution_str)
+		print("Frame: "+str(frameCounter))
+		print("Average FPS: "+str(average_fps))
+
+	# show the frame and update the FPS counter
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(delay=1) ## 1 milliseconds
+	if key==27:    # Esc key to stop   ####  & 0xFF == ord('q'):
+		break
+
 
 # When everything done, release the capture
-cap.release()
+stream.release()
 cv2.destroyAllWindows()
---
+++ nbs/drawing.py
@@ -0,0 +1,8 @@
+# Put text
+cv2.putText(img       = frame,
+			text      = resolution_str,
+			org       = (50, 100) ,
+			fontFace  = cv2.FONT_HERSHEY_SIMPLEX,
+			fontScale = 1,  
+			color     = (255, 255, 255),
+			thickness = 2)
